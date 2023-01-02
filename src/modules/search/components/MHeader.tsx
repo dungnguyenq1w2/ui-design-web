62ec5eae2dffc4cdd87f4cb8f336a413f6b3c545
Nguyễn Tấn Dũng <dungnguyenq1w2@gmail.com>
2023-01-02 18:41:43 +0700

    update

--- src/modules/search/components/MHeader.tsx
+++ src/modules/search/components/MHeader.tsx
@@ -1,7 +1,6 @@
 import { FormEvent, useEffect, useRef, useState } from 'react'
 import { createSearchParams, useNavigate } from 'react-router-dom'
 
-import { Search } from '../model/Search'
 import MSuggestionSearch from './MSuggestionSearch'
 export interface IMHeaderProps {
     data: any
@@ -23,11 +22,12 @@ export interface IMHeaderProps {
 
 export default function MHeader({ data }: IMHeaderProps) {
     //#region Data
+    const submitButtonRef = useRef<any>(null)
     const inputRef = useRef<any>(null)
     const navigate = useNavigate()
     const [input, setInput] = useState('')
     const [isSuggestionModalOpen, setIsSuggestionModalOpen] = useState(false)
-    const [suggestions, setSuggestions] = useState<Search[]>([])
+    const [suggestions, setSuggestions] = useState<any[]>([])
     // const [searchParams, setSearchParams] = useSearchParams()
     //#endregion
 
@@ -40,7 +40,7 @@ export default function MHeader({ data }: IMHeaderProps) {
 
     const handleSearchSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        const inputValue = input.trim()
+        const inputValue = input.trim().toLowerCase()
         // if (!inputValue) return
         // setSearchParams()
         // navigate(`/search?keyword=${inputValue}`)
@@ -55,14 +55,24 @@ export default function MHeader({ data }: IMHeaderProps) {
 
     const handleInputChange = (e: any) => {
         setInput(e.target.value)
-        let newSuggestions = []
+        let newSuggestions: any = []
+
         if (e.target.value) {
-            const inputValue = e.target.value.trim()
-            newSuggestions = [...data].filter((item) =>
-                item?.title.toLowerCase().includes(inputValue),
-            )
+            const inputValue = e.target.value.trim().toLowerCase()
+            newSuggestions = [...data]
+                .filter((e) => {
+                    if (e?.title) return e.title.toLowerCase().includes(inputValue)
+                    else if (e?.name) return e.name.toLowerCase().includes(inputValue)
+                })
+                .map((e) => ({ id: e.id, title: e?.title ? e.title : e.name }))
+        } else {
+            newSuggestions = []
         }
-        setSuggestions(newSuggestions)
+        const result: any[] = [...newSuggestions].filter(function (item: any, pos: number) {
+            return newSuggestions.findIndex((e: any) => e.title === item.title) == pos
+        })
+
+        setSuggestions(result)
         setIsSuggestionModalOpen(true)
     }
     //#endregion
@@ -76,7 +86,7 @@ export default function MHeader({ data }: IMHeaderProps) {
                 strokeWidth={1.5}
                 stroke='currentColor'
                 className='w-6 h-6 mr-2'
-                onClick={() => navigate(-1)}
+                onClick={() => navigate('/')}
             >
                 <path
                     strokeLinecap='round'
@@ -89,7 +99,7 @@ export default function MHeader({ data }: IMHeaderProps) {
                 onSubmit={handleSearchSubmit}
                 className='flex flex-1 bg-gray-200 p-2 rounded-full'
             >
-                <button type='submit' title='Search'>
+                <button type='submit' title='Search' ref={submitButtonRef}>
                     <svg
                         xmlns='http://www.w3.org/2000/svg'
                         fill='none'
@@ -134,6 +144,9 @@ export default function MHeader({ data }: IMHeaderProps) {
                     onPress={(title: string) => {
                         setInput(title)
                         setIsSuggestionModalOpen(false)
+                        setTimeout(() => {
+                            return submitButtonRef?.current ? submitButtonRef.current.click() : null
+                        }, 50)
                     }}
                 />
             )}
